f8dac964e745693e3993f077ee3ea9ff77f5c950
Waqas Akhtar <mwaqasakhtar101@gmail.com>
2022-11-15 16:32:49 +0500

    deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> My name is Waqas Akhtar</h1>
+     <h2>I am deploying this on github pages</h2>
+    </div>
+  );
+}
+
+export default App;
